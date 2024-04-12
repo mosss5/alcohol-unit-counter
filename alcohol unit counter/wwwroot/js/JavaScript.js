e3b3bb3b5be7c4e7be3ec10170dd9f58fb930d79
mosss5 <rupertmorriss@gmail.com>
2024-04-12 09:35:23 +0100

    start of css

--- alcohol unit counter/wwwroot/js/JavaScript.js
+++ alcohol unit counter/wwwroot/js/JavaScript.js
@@ -1,51 +1,49 @@
-﻿var total = 0
-var bacTotal = 0
+﻿const bacElement = document.getElementById('bac');
+const unitElement = document.getElementById('unit');
+const mlElement = document.getElementById('ml');
 
 function myFunction() {
-        var input, filter, ul, li, a, i, txtValue;
-input = document.getElementById("myInput");
-filter = input.value.toUpperCase();
-ul = document.getElementById("myUL");
-li = ul.getElementsByTagName("li");
-for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-txtValue = a.textContent || a.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    li[i].style.display = "";
-            } else {
-    li[i].style.display = "none";
-            }
-        }
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+           }
     }
+}
 
 
 function addAlcohol() {
         // Select all input elements with the name "quantity" within <li> elements
-        const inputElements = document.querySelectorAll('li input[name="quantity"]');
+        const inputElements = document.querySelectorAll('li input[name="quantity"],li input[name="percentage"]');
 
-    let totalAlcohol = 0;
+        let totalAlcohol = 0;
 
-    // Iterate through the input elements and calculate the total alcohol content
-    inputElements.forEach(function (inputElement) {
-            const percentage = parseFloat(inputElement.id.replace('%', ''));
-    // Get the quantity entered by the user
-    const quantity = parseFloat(inputElement.value) || 0;
-    // Calculate alcohol content based on percentage and quantity
-    const alcoholContent = (percentage / 1000) * quantity;
-    // Add to the total alcohol content
-    totalAlcohol += alcoholContent;
-        });
+        // Iterate through the input elements and calculate the total alcohol content
+        for (let i = 0; i < inputElements.length; i += 2) {
+            const percentage = parseFloat(inputElements[i].value) || 0;
+            const quantity = parseFloat(inputElements[i + 1].value) || 0;
+            const alcoholContent = (percentage / 1000) * quantity;
+            totalAlcohol += alcoholContent;
+        }
 
-    bloodAlcoholContent(totalAlcohol);
-    document.getElementById("alcohol").innerHTML = "<p>" + totalAlcohol + "</p>";
+        bloodAlcoholContent(totalAlcohol);
+        unitElement.textContent = 'Units:' + totalAlcohol;
             
     }
 
 
 
 
-    function bloodAlcoholContent(alcoholUnits) {
-        let alcoholWeight = alcoholUnits * 8;
+function bloodAlcoholContent(alcoholUnits) {
+    let alcoholWeight = alcoholUnits * 8;
     userWeight = document.getElementById("inputWeight");
     const weight = parseFloat(userWeight.value) || 0;
     const selectedGender = document.querySelector('input[name="gender"]:checked');
@@ -61,14 +59,17 @@ function addAlcohol() {
 
     bloodAlcohol = 100 * alcoholWeight / ((weight*1000) * r);
 
-    document.getElementById("bac").innerHTML = "<p>" + bloodAlcohol + "</p>";
-    }
+    bacElement.textContent = 'BAC:' + bloodAlcohol;
+}
+
+function convertPintsToML() {
+
+    const pints = parseFloat(document.getElementById("pints").value) || 0;
+
+    const milliliters = pints * 568;
+
+    mlElement.textContent = 'ml:' + milliliters;
+}
 
 
 
-
-
-
-
-    document.getElementById("bac").innerHTML = "<p>" + bacTotal + "</p>";
-    document.getElementById("alcohol").innerHTML = "<p>" + total + "</p>";
